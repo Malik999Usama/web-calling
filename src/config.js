26fb76495fb25a8bc45619fcdd7173a85ce4a094
Malik999Usama <malik999usama@gmail.com>
2022-05-23 10:33:57 +0500

    commit

---
+++ src/config.js
@@ -0,0 +1,61 @@
+export const messages = {
+  login:
+    "Login as any user on this computer and another user on another computer.",
+  create_session: "Creating a session...",
+  connect: "Connecting...",
+  connect_error:
+    "Something went wrong with the connection. Check internet connection or user info and try again.",
+  login_as: "Logged in as ",
+  title_login: "Choose a user to login with:",
+  title_callee: "Choose users to call:",
+  calling: "Calling...",
+  webrtc_not_avaible: "WebRTC is not available in your browser",
+  no_internet: "Please check your Internet connection and try again",
+};
+
+export const credentials = {
+  appId: 6255,
+  authKey: "Q4c6fwxZvATxheD",
+  authSecret: "TVgVJwSuBhSfFeL",
+};
+
+export const appConfig = {
+  debug: { mode: 1 },
+  // videocalling: {
+  //   answerTimeInterval: 30,
+  //   dialingTimeInterval: 5,
+  //   disconnectTimeInterval: 35,
+  //   statsReportTimeInterval: 5
+  // }
+};
+
+export const users = [
+  // {
+  //   id: 5849874,
+  //   name: "malikUsama",
+  //   login: "malikusama",
+  //   password: "malik999usama123",
+  //   // color: "#34ad86",
+  // },
+  {
+    id: 5849875,
+    name: "Faisal Jamil",
+    login: "faisaljamil",
+    password: "faisaljamilofficial123",
+    color: "#077988",
+  },
+  // {
+  //   id: 5849877,
+  //   name: "connecty",
+  //   login: "connecty",
+  //   password: "connecty@123",
+  //   // color: "#13aaae",
+  // },
+  {
+    id: 5850065,
+    name: "New Connecty",
+    login: "newconnecty",
+    password: "newconnecty123",
+    color: "#056a96",
+  },
+];
